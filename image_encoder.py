06e99421a60d96df1c45415c8bf742c9b0212436
BOT-Jude <96182758+BOT-Jude@users.noreply.github.com>
2023-11-29 19:02:53 +0000

    Added sample data from CelebA dataset.

--- image_encoder.py
+++ image_encoder.py
@@ -53,48 +53,56 @@ def is_image(filename):
     return filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
 
 
-class LookupEncoder:
+class CelebALookupEncoder:
     def __init__(self, embedding_dim):
         assert embedding_dim == 50
 
     def __call__(self, data_root):
 
-        # separate the data into images and characteristic data
+        # find image paths and landmarks.txt and attributes.txt
         paths = [os.path.join(data_root, file_path) for file_path in os.listdir(data_root)]
         image_paths = self.image_paths = list(filter(is_image, paths))
-        txt_files = list(filter(lambda x: x.endswith(".txt"), paths))
-        assert len(txt_files) == 1, \
-            "There must only be one txt file in the data directory"
-        characteristic_file = txt_files[0]
+        landmarks_file = list(filter(lambda x: x.endswith("list_landmarks_align_celeba.txt"), paths))[0]
+        attributes_file = list(filter(lambda x: x.endswith("list_attr_celeba.txt"), paths))[0]
 
-        # read data from characteristic_file into dictionary
-        self.photo_characteristics = {}
-
-        with open(characteristic_file, "r") as data:
-            data.readline()
-            data.readline()
-            for line in data:
+        # create dictionary for landmark data
+        self.photo_landmarks = {}
+        with open(landmarks_file, "r") as f:
+            f.readline()
+            f.readline()
+            for line in f:
                 line = line.split()
                 img_name = line.pop(0)
-                descriptors = torch.tensor([int(c) for c in line], dtype=torch.float)
 
                 # extract and normalize landmark coordinates
-                landmarks = descriptors[-10:]
+                landmarks = torch.tensor([int(c) for c in line], dtype=torch.float)
                 landmarks[[0, 2, 4, 6, 8]] = (landmarks[[0, 2, 4, 6, 8]] / 178.0) - 0.5
                 landmarks[[1, 3, 5, 7, 9]] = (landmarks[[1, 3, 5, 7, 9]] / 218.0) - 0.5
 
-                # extract and map descriptors (-1, 1) -> (0, 1)
-                attributes = descriptors[:-10]
+                self.photo_landmarks[img_name] = landmarks
+
+        # create dictionary for attribute data
+        self.photo_attributes = {}
+        with open(attributes_file, "r") as f:
+            f.readline()
+            f.readline()
+            for line in f:
+                line = line.split()
+                img_name = line.pop(0)
+
+                # extract and map attributes (-1, 1) -> (0, 1)
+                attributes = torch.tensor([int(c) for c in line], dtype=torch.float)
                 attributes = (attributes + 1.0) / 2.0
 
-                self.photo_characteristics[img_name] = (attributes, landmarks)
+                self.photo_attributes[img_name] = attributes
 
         # select corresponding image data for embeddings matrix
         embeddings = torch.zeros(len(image_paths), 50)
 
         for i, path in enumerate(image_paths):
             img_name = path.split("/")[-1]
-            attributes, landmarks = self.photo_characteristics[img_name]
+            landmarks = self.photo_landmarks[img_name]
+            attributes = self.photo_attributes[img_name]
             embeddings[i] = torch.cat([attributes, landmarks])
 
         return embeddings

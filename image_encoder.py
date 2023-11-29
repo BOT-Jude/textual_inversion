7a042320bcb172f391884038fc3469e584e14d98
BOT-Jude <96182758+BOT-Jude@users.noreply.github.com>
2023-11-29 16:57:33 +0000

    Fix bug: Image Encoder embeddings are length 2048 not 1000

--- image_encoder.py
+++ image_encoder.py
@@ -19,13 +19,13 @@ def normalize_img(image, img_size):
 
 class ImageEncoder:
     def __init__(self, embedding_dim):
-        assert embedding_dim == 1000
+        assert embedding_dim == 2048
         model = {m[0]: m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}["Xception"]
         self.pre_trained_model = model(include_top=False, pooling='avg', input_shape=(128, 128, 3))
         self.pre_trained_model.trainable = False
 
     def __call__(self, image_paths):
-        embeddings = torch.zeros(len(image_paths), 1000)
+        embeddings = torch.zeros(len(image_paths), 2048)
         for i, path in enumerate(image_paths):
             image = tf.keras.utils.load_img(path)
             image = normalize_img(image, tf.constant([128, 128]))
